acc8f942341bd6f34b73eddda3b1497de2cfc1e4
Mostafa <sasasayed1231@gmail.com>
2026-01-18 21:44:13 +0200

    edit

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,13 +1,18 @@
-import { NextResponse } from "next/server";
-import {
-  cleanupExpiredTokens,
-  cleanupUnverifiedUsers,
-} from "@/features/cron/cleanup.service";
+// src/app/api/cron/cleanup/route.ts
+import { headers } from "next/headers";
+import { cleanupExpiredTokens, cleanupUnverifiedUsers } from "@/features/cron/cleanup.service";
 
+export async function POST() {
+  const h = headers();
+  const ua = (await h).get("user-agent") || "";
 
-export async function GET() {
+  // تأكيد إنه من Vercel
+  if (!ua.toLowerCase().includes("vercel")) {
+    return new Response("Forbidden", { status: 403 });
+  }
+
   await cleanupExpiredTokens();
   await cleanupUnverifiedUsers();
 
-  return NextResponse.json({ success: true });
+  return Response.json({ success: true });
 }
