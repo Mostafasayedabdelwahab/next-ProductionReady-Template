bbdc969793272473e40bcd808bbf3982fb307758
Mostafa <sasasayed1231@gmail.com>
2026-01-18 19:48:29 +0200

    Edit

--- src/app/api/cron/cleanup/route.ts
+++ src/app/api/cron/cleanup/route.ts
@@ -5,6 +5,7 @@ import {
 } from "@/features/cron/cleanup.service";
 
 export async function GET() {
+  console.log("CRON HIT", new Date().toISOString());
   await cleanupExpiredTokens();
   await cleanupUnverifiedUsers();
 
