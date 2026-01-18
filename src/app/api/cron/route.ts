37dac580ea11456eb67a49efee396461bcbd5744
Mostafa <sasasayed1231@gmail.com>
2026-01-18 23:16:31 +0200

    edit

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,21 +1,19 @@
-// src/app/api/cron/cleanup/route.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import {
   cleanupExpiredTokens,
   cleanupUnverifiedUsers,
 } from "@/features/cron/cleanup.service";
 
-export async function POST(request: Request) {
+export async function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
-
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response("Unauthorized", { status: 401 });
+    return new Response("Unauthorized", {
+      status: 401,
+    });
   }
-
   await cleanupExpiredTokens();
   await cleanupUnverifiedUsers();
 
-  return Response.json({
-    success: true,
-    ranAt: new Date().toISOString(),
-  });
+  return NextResponse.json("Cron Job ", { status: 200 });
 }
